6d0979aaf146f2c63e669851055788c810f34761
patrykbasiak <patrykbasiak9@gmail.com>
2021-07-03 12:21:15 +0200

    cockatils-DB

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        voluptatem praesentium nisi illo magni placeat repellat quisquam iste
+        quo amet. Maxime quae reiciendis nesciunt delectus animi soluta adipisci
+        voluptas amet, illum enim laboriosam facilis ex quas maiores pariatur
+        odit modi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
